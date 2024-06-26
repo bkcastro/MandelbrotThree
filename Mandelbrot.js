9308e011463458832658a3e72acb15676eb65067
Brandon Castro <C1brandon626@gmail.com>
2024-06-25 22:19:58 -0700

    Initial commit

---
+++ Mandelbrot.js
@@ -0,0 +1,96 @@
+import * as THREE from 'three';
+
+const vertexShader = `
+    varying vec3 vPosition;
+
+    void main() {
+        vPosition = position;
+        gl_PointSize = 1.25;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+`;
+
+const fragmentShader = `
+    precision mediump float;
+    varying vec3 vPosition;
+    uniform vec3 uCenterColor;
+    uniform vec3 uOuterColor;
+
+    void main() {
+        vec2 coord = gl_PointCoord * 2.0 - 1.0;
+        float distance = length(coord);
+
+        if (distance > 1.0) {
+            discard;
+        }
+
+        float distanceFromCenter = length(vPosition - vec3(0, 0, 0));
+        float gradient = distanceFromCenter;
+        vec3 color = mix(uCenterColor, uOuterColor, gradient);
+
+        gl_FragColor = vec4(color, 1.0);
+    }
+`;
+
+
+function generateMandelbrot3D(width, height, depth, maxIterations) {
+    const geometry = new THREE.BufferGeometry();
+    const vertices = [];
+
+    for (let x = -width / 2; x < width / 2; x++) {
+        for (let y = -height / 2; y < height / 2; y++) {
+            for (let z = -depth / 2; z < depth / 2; z++) {
+                let cx = x / width * 4 - 2;
+                let cy = y / height * 4 - 2;
+                let cz = z / depth * 4 - 2;
+
+                let iteration = 0;
+                let zx = 0, zy = 0, zz = 0;
+                while (Math.abs(zx) < .01 && Math.abs(zy) < .01 && Math.abs(zz) < .01 && iteration < maxIterations) {
+                    let xtemp = zx * zx - zy * zy - zz * zz + cx;
+                    let ytemp = 2 * zx * zy + cy;
+                    let ztemp = 2 * zx * zz + cz;
+                    if (zx === xtemp && zy === ytemp && zz === ztemp) {
+                        iteration = maxIterations;
+                        break;
+                    }
+                    zx = xtemp;
+                    zy = ytemp;
+                    zz = ztemp;
+                    iteration++;
+                }
+
+                if (iteration < maxIterations) {
+                    vertices.push(x, y, z);
+                }
+            }
+        }
+    }
+
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+    return geometry;
+}
+
+export class MandelbrotCube extends THREE.Object3D {
+    constructor() {
+        super();
+
+        const mandelbrotGeometry = generateMandelbrot3D(100, 100, 100, 100);
+        const particleMaterial = new THREE.ShaderMaterial({
+            uniforms: {
+                uCenterColor: { value: new THREE.Color(0xff0000) },
+                uOuterColor: { value: new THREE.Color(0x000000) },
+            },
+            vertexShader: vertexShader,
+            fragmentShader: fragmentShader,
+        });
+        this.points = new THREE.Points(mandelbrotGeometry, particleMaterial);
+        this.add(this.points);
+    }
+
+    update(time) {
+        this.rotation.x = Math.sin(time / 20);
+        this.rotation.y = Math.sin(time / 10);
+        this.rotation.z = Math.sin(time / 20);
+    }
+}
---
+++ main.js
@@ -0,0 +1,69 @@
+// By Brandon Castro 
+import './style.css'
+import * as THREE from 'three'
+import { XRButton, OrbitControls } from 'three/examples/jsm/Addons.js'
+import Stats from 'three/examples/jsm/libs/stats.module.js';
+import { MandelbrotCube } from './Mandelbrot';
+
+const stats = new Stats();
+stats.showPanel(0)
+document.body.appendChild(stats.dom);
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x808080);
+
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.0001, 100);
+camera.position.set(0, 1, 2);
+
+scene.add(new THREE.HemisphereLight(0xbcbcbc, 0xa5a5a5, 3));
+
+// Helper 
+scene.add(
+  new THREE.AxesHelper(1)
+);
+
+// renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+renderer.xr.enabled = true;
+document.body.appendChild(renderer.domElement);
+
+document.body.appendChild(XRButton.createButton(renderer));
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target = new THREE.Vector3(0, 1.5, -1);
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+// 
+
+const mandelbort = new MandelbrotCube();
+mandelbort.scale.multiplyScalar(1 / 150);
+mandelbort.position.set(0, 1.5, -1);
+scene.add(mandelbort);
+
+// 
+
+// Game loop 
+
+const clock = new THREE.Clock();
+
+function animate() {
+  stats.begin();
+
+  const elapstedTime = clock.getElapsedTime();
+  mandelbort.update(elapstedTime);
+  controls.update();
+  renderer.render(scene, camera);
+
+  stats.end();
+}
+
+animate(); 
